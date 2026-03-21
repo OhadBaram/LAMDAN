c4006ae18d00612679f76de0e6c9dfffbe4ca2f3
avivbaramamirim-crypto <aviv.baram.amirim@gmail.com>
2026-03-21 13:00:39 +0200

    fix: add AppProvider and UserSettingsProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AppProvider } from '../contexts/AppContext';
+import { UserSettingsProvider } from '../contexts/UserSettingsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
-  <App />
+  <UserSettingsProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </UserSettingsProvider>
 </React.StrictMode>);
